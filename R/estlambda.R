233e28316638503cccb2e59357145c7131517d2c
Chris-Wills <chriswillsem@gmail.com>
2020-06-03 13:41:27 +0100

    changed est_lambda function

--- R/estlambda.R
+++ R/estlambda.R
@@ -1,74 +1,23 @@
-#' Estimate Lambda
+#' genomic_lambda
 #'
-#' This function takes a vector of P-values and estimates the genomic inflation factor
+#' This function takes a vector of P-values, Z-scores or Chi-squared statistics and estimates the genomic inflation factor
 
 #' @export
 
-"estlambda" <- function(data, plot=FALSE, proportion=1.0,
-                        method="regression", filter=TRUE, df=1,... ) {
-        data <- data[which(!is.na(data))]
-        if (proportion>1.0 || proportion<=0)
-                stop("proportion argument should be greater then zero and less than or equal to one")
-
-        ntp <- round( proportion * length(data) )
-        if ( ntp<1 ) stop("no valid measurements")
-        if ( ntp==1 ) {
-                warning(paste("One measurement, lambda = 1 returned"))
-                return(list(estimate=1.0, se=999.99))
-        }
-        if ( ntp<10 ) warning(paste("number of points is too small:", ntp))
-        if ( min(data)<0 ) stop("data argument has values <0")
-        if ( max(data)<=1 ) {
-#		lt16 <- (data < 1.e-16)
-#		if (any(lt16)) {
-#			warning(paste("Some probabilities < 1.e-16; set to 1.e-16"))
-#			data[lt16] <- 1.e-16
-#		}
-                data <- qchisq(data, 1, lower.tail=FALSE)
-        }
-        if (filter)
-        {
-                data[which(abs(data)<1e-8)] <- NA
-        }
-        data <- sort(data)
-        ppoi <- ppoints(data)
-        ppoi <- sort(qchisq(ppoi, df=df, lower.tail=FALSE))
-        data <- data[1:ntp]
-        ppoi <- ppoi[1:ntp]
-#	s <- summary(lm(data~offset(ppoi)))$coeff
-#       bug fix thanks to Franz Quehenberger
-
-        out <- list()
-        if (method=="regression") {
-                s <- summary( lm(data~0+ppoi) )$coeff
-                out$estimate <- s[1,1]
-                out$se <- s[1,2]
-        } else if (method=="median") {
-                out$estimate <- median(data, na.rm=TRUE)/qchisq(0.5, df)
-                out$se <- NA
-        } else if (method=="KS") {
-                limits <- c(0.5, 100)
-                out$estimate <- estLambdaKS(data, limits=limits, df=df)
-                if ( abs(out$estimate-limits[1])<1e-4 || abs(out$estimate-limits[2])<1e-4 )
-                        warning("using method='KS' lambda too close to limits, use other method")
-                out$se <- NA
-        } else {
-                stop("'method' should be either 'regression' or 'median'!")
-        }
-
-        if (plot) {
-                lim <- c(0, max(data, ppoi,na.rm=TRUE))
-#		plot(ppoi,data,xlim=lim,ylim=lim,xlab="Expected",ylab="Observed", ...)
-                oldmargins <- par()$mar
-                par(mar=oldmargins + 0.2)
-                plot(ppoi, data,
-                     xlab=expression("Expected " ~ chi^2),
-                     ylab=expression("Observed " ~ chi^2),
-                     ...)
-                abline(a=0, b=1)
-                abline(a=0, b=out$estimate, col="red")
-                par(mar=oldmargins)
-        }
-
-        out
+"genomic_lambda" <- function(data, input) {
+  out <- list()
+  if (input == "P") {
+    data_chi <- qchisq(data, 1, lower.tail=FALSE)
+    out$lambda <- median(data_chi, na.rm=TRUE)/qchisq(0.5,1)
+  }
+  else if (input == 'Z'){
+    out$lambda <- median(data^2, na.rm=TRUE)/qchisq(0.5,1)
+  }
+  else if (input == 'chi') {
+    out$lambda <- median(data, na.rm=TRUE)/qchisq(0.5,1)
+  }
+  else {
+    stop("'input' should be either 'P', 'Z' or 'chi'!")
+  }
+  out
 }
